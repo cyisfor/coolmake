a8db6071c399ae5ab213f74ed763cb39100e123a
user <nobody>
2017-11-07 17:54:28 +0000

    generated file ugh

--- o/lostdeps.d
+++
@@ -1,5 +0,0 @@
-o/testcompiledep.lo src/testcompiledep.c o/lostdeps.d: o/gen1.h
-o/testcompiledep.d ./o/gen1.h o/lostdeps.d: o/gen2.h
-o/testcompiledep.d src/testcompiledep.c o/lostdeps.d: doesntwork.h
-o/testcompiledep.d src/testcompiledep.c o/lostdeps.d: doesntwork.h
-o/testcompiledep.lo src/testcompiledep.c o/lostdeps.d: doesntwork.h
